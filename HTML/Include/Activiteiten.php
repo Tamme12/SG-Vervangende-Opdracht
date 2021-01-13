611b3f4f5aa9ff47f3533249b7f7541b25952f39
MichelObama <72194919+MichelObama@users.noreply.github.com>
2021-01-13 16:33:25 +0100

    Home pagina gemaakt
    de home pagina is er, compleet met een header, RSS feed en een activiteiten pagina.

---
+++ HTML/Include/Activiteiten.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+<body>	
+
+	<img class="actfoto"src="Afbeeldingen/Activiteiten.png" alt="Activiteiten" title="Acticiteiten">
+	
+	<div class="activiteit">
+		<img class="afoto" src="Afbeeldingen/Activiteiten/Comic.jpg" alt="activiteitfoto" title="activiteitfoto">
+		<div class="contentdiv">
+			<div class="titeldiv">
+				<a href="Artiekelen/Comic-identity-of-the-body-basic-level.php"><h2 class="titel">Comic identity of the body basic level</h2></a>
+			</div>
+			<div class="textdiv"><p class="text">Ik ben mijn artistieke studies begonnen vanuit het klassieke theater en al snel raakte ik geïnteresseerd in het gebaartheater,
+			body arts en deze globale en onbegrensde mogelijkheden en manieren van expressie. De personages die ik aan het begin van mijn carrière heb gespeeld,
+			hadden al een fysieke uitvoering en ik probeerde onmiddellijk een manier te vinden om het gebruik van de woorden te beperken door de intenties van de personages 
+			uit de bewegingen te spelen... <a href="Comic-identity-of-the-body-basic-level.php">Verder Lezen.</a></p></div>
+		</div>
+	</div>
+	
+	<div class="activiteit">
+		<img class="afoto" src="Afbeeldingen/Activiteiten/Humanrightsday2020.png" alt="activiteitfoto" title="activiteitfoto">
+		<div class="contentdiv">
+			<div class="titeldiv">
+				<a href="Artiekelen/Human-rights-day-2020.php"><h2 class="titel">Human Rights Day 2020</h2></a>
+			</div>
+			<div class="textdiv"><p class="text">10 december is de Dag van de Mensenrechten, ter herdenking van de Universele Verklaring van de Rechten van de Mens 
+			(UVRM) uit 1948. Dit historische document van de Verenigde Naties specificeert dat alle mensen dezelfde onvervreemdbare rechten hebben,
+			ongeacht achtergrond, afkomst of ander verschil. Het thema van dit jaar: "Herstellen"... <a href="Artiekelen/Human-rights-day-2020.php">Verder Lezen.</a></p></div>
+		</div>
+	</div>
+
+	<div class="activiteit">
+		<img class="afoto" src="Afbeeldingen/Activiteiten/Placeholder.png" alt="activiteitfoto" title="activiteitfoto">
+		<div class="contentdiv">
+			<div class="titeldiv">
+				<a href="Artiekelen/leegartiekel.php"><h2 class="titel">Vul hier je titel in.</h2></a>
+			</div>
+			<div class="textdiv"><p class="text">Vul hier je text in. <a href="Artiekelen/leegartiekel.php">Verder Lezen.</a></p></div>
+		</div>
+	</div>
+</body>
+</html> 
---
+++ HTML/Include/artiekelheader.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+<body>
+	<header> 
+		<a href="../index.php"><img class="logo" src="../Afbeeldingen/icon.png" alt="Logo" title="Logo"></a>
+		<ul class="headul">
+			<li class="list"><a href="#">Over Ons</a></li>
+			<li class="list"><a href="../contactpagina.html">Contact</a></li>
+		</ul>
+	</header>
+</body>
+</html> 
